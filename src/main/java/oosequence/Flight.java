ee935aa200c2ea62b7ff75a188b500b68088afab
Soila Pertet <nicolepertet@gmail.com>
2023-07-24 10:20:05 +0300

    Initialise Flight class

---
+++ src/main/java/oosequence/Flight.java
@@ -0,0 +1,23 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+    // Define the instance variables
+    Date arrival;
+    Date departure;
+
+    // Define class constructors
+    public Flight(Date departure, Date arrival){
+
+    }
+
+    public Flight(Flight flight){
+
+    }
+
+    // Define class methods
+    long length(){
+        return 0;
+    }
+}
