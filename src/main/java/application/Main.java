4b669b30510bc1be578e0de5787294a7df9521a2
Soila Pertet <nicolepertet@gmail.com>
2023-08-02 08:55:24 +0300

    Write code for addFlight method which adds flights while checking for overlap

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -18,8 +18,13 @@ public class Main extends Application {
      */
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
-        VBox root = loader.load(new FileInputStream(
-                "src/main/java/application/GradeCalculatorView.fxml"));
+        VBox root = loader.load(new FileInputStream("src/main/java/application/GradeCalculatorView.fxml"));
+
+        // Get the controller after loading FXML loader file (we need to type cast)
+        GradeCalculatorController controller = (GradeCalculatorController)loader.getController();
+
+        // "Give" the primary stage to the controller; allows us to access the main stage of the application
+        controller.applicationStage = primaryStage;
 
         Scene scene = new Scene(root, 400, 400);
         primaryStage.setTitle("Soila Pertet's Grade Calculator");
--- src/main/java/oosequence/Itinerary.java
+++ src/main/java/oosequence/Itinerary.java
@@ -18,12 +18,31 @@ public class Itinerary {
         return "";
     }
     ArrayList<Flight> getFlights(){
-        return null;
+        return flights;
     }
 
     // Define class methods
     void addFlight(Flight flight){
+        if(flights.isEmpty()) {
+            flights.add(flight);
+        } else {
+            boolean overlap = false;
+            boolean flightAdded = false;
+            int i = flights.size() - 1;
 
+            while(i >= 0 && !overlap && !flightAdded) {
+                Flight current = flights.get(i);
+
+                if(flight.getDeparture().after(current.getDeparture()) && flight.getDeparture().after(current.getArrival())) {
+                    flights.add(flight);
+                    flightAdded = true;
+                } else if(flight.getDeparture().after(current.getDeparture()) && flight.getDeparture().before(current.getArrival())) {
+                    overlap = true;
+                }
+
+                i--;
+            }
+        }
     }
 
     long getTotalLayover(){
